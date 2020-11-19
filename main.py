b5f47cd851fd8baa49922039b544ac8ead0d86c2
Mateusz Szymiec <mattszymiec@gmail.com>
2020-11-20 00:22:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,22 @@ import sys
 import pygame
 
 def run():
-    pygame.init()
+    pygame.init()   # Initialize pygame.
 
     screen_width = 500
     screen_height = 500
     bg_color = (255, 255, 255)
     
-    # Create a window on the screen.
-    screen = pygame.display.set_mode([screen_width, screen_height])
+    screen = pygame.display.set_mode((screen_width, screen_height)) # Create a window.
     pygame.display.set_caption("Snake")
 
     while True:
-
-        # Wait for a key to be pressed
-        for event in pygame.event.get():
+        for event in pygame.event.get():    # Look for a user action.
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)   # Fill the background.
         
-        # Update the screen
-        pygame.display.flip()
+        pygame.display.flip()   # Update the screen.
 
 run()
