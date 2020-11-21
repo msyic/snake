16e4f9b17e49db5c39c2d9096fe8bf8053954f7c
Mateusz Szymiec <mattszymiec@gmail.com>
2020-11-21 14:23:01 +0100

    Add snake movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.math import Vector2
 from settings import Settings
 from snack import Snack
 from snake import Snake
@@ -17,11 +18,25 @@ def main():
     snack = Snack(screen)
     snake = Snake(screen)
 
+    SCREEN_UPDATE = pygame.USEREVENT
+    pygame.time.set_timer(SCREEN_UPDATE, 100)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == SCREEN_UPDATE:
+                snake.move_snake()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    snake.direction = Vector2(0, -1)
+                if event.key == pygame.K_DOWN:
+                    snake.direction = Vector2(0, 1)
+                if event.key == pygame.K_LEFT:
+                    snake.direction = Vector2(-1, 0)
+                if event.key == pygame.K_RIGHT:
+                    snake.direction = Vector2(1, 0)
 
         screen.fill(settings.bg_color)
 
